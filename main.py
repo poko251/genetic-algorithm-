a8fc607f3c9e3b98f06d98a921823548d5bd17d0
Michal_laptop <michal200323@gmail.com>
2026-01-17 10:48:31 +0100

    add vis

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from GeneticAlgoritm import GeneticAlgoritm
+import matplotlib.pyplot as plt
+import numpy as np
 
-algorytm = GeneticAlgoritm(populationSize=100, chromosomeSize=10, elitismSize=40, epsilon=0.001)
+algorytm = GeneticAlgoritm(populationSize=100, chromosomeSize=7, elitismSize=2, epsilon=0.001)
 
 
-najlepszy = algorytm.run(epsilon=0.001, maxGenerations=1000)
+najlepszy = algorytm.run(epsilon=0.001, maxGenerations=100) #zwraca zdekodowaną wartość
 
-print(najlepszy)
+
+
+def vis(point): #wizualizacja najlepszego osobnika z ostatniej populacji, na wykresie funkcji
+
+    x = np.linspace(-50, 50)
+    
+    y = -1*(x**2) - 2*(x) + 10
+
+    y_point = -1*(point**2) - 2*(point) + 10
+
+    plt.scatter(point, y_point, color="red")
+
+
+    plt.axhline(0, color="black")
+    plt.axvline(0, color="black")
+
+    plt.plot(x, y)
+    plt.show()
+
+
+vis(najlepszy)
